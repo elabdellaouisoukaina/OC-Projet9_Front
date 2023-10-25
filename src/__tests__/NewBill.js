bcc947d7bef91297cf4bd3db1c713142b71f9d65
Soukaïna EL Abdellaoui <elabdellaoui.soukaina@gmail.com>
2023-10-25 14:52:58 +0200

    [Tests unitaires et d'intégration] container/NewBill - couverture : tout les inputs du form fonctionnels + handleSubmit()

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -99,6 +99,69 @@ describe("Given I am connected as an employee", () => {
           userEvent.click(uploadFileInput, { target: { files: [pngFile] } });
           expect(pngFile.type === 'image/png').toBeTruthy()
         })
+
+        describe('When I fill correctly the form and click on the "Envoyer" button', () => {
+          test('Then the form should be successfully submitted', () => {
+            //Teste que le click sur le bouton appelle la bonne fonction
+            const onNavigate = (pathname) => {
+              document.body.innerHTML = ROUTES({ pathname })
+            }
+            const store = null
+            const newBill = new NewBill({ document, onNavigate, store, localStorage })
+
+            const inputExpenseType = screen.getByTestId("expense-type");
+            fireEvent.change(inputExpenseType, { target: { value: "Transports" } });
+            expect(inputExpenseType.value).toBe("Transports");
+
+            const inputExpenseName = screen.getByTestId("expense-name");
+            fireEvent.change(inputExpenseName, { target: { value: "Test" } });
+            expect(inputExpenseName.value).toBe("Test");
+
+            const inputExpenseDate = screen.getByTestId("datepicker");
+            fireEvent.change(inputExpenseDate, { target: { value: "2018-01-04" } });
+            expect(inputExpenseDate.value).toBe( "2018-01-04");
+
+            const inputExpenseAmount = screen.getByTestId("amount");
+            fireEvent.change(inputExpenseAmount, { target: { value: "0" } });
+            expect(inputExpenseAmount.value).toBe("0");
+
+            const inputExpenseVat = screen.getByTestId("vat");
+            fireEvent.change(inputExpenseVat, { target: { value: "0" } });
+            expect(inputExpenseVat.value).toBe("0");
+
+            const inputExpensePct = screen.getByTestId("pct");
+            fireEvent.change(inputExpensePct, { target: { value: "0" } });
+            expect(inputExpensePct.value).toBe("0");
+
+            const inputExpenseCommentary = screen.getByTestId("commentary");
+            fireEvent.change(inputExpenseCommentary, { target: { value: "test test test" } });
+            expect(inputExpenseCommentary.value).toBe("test test test");
+            
+            const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+            let uploadFileInput = screen.getByTestId("file")
+            const jpegFile = new File([new Blob()], 'test.jpeg', {
+              type: 'image/jpeg',
+            });
+            uploadFileInput.addEventListener('click', handleChangeFile) 
+            userEvent.click(uploadFileInput, { target: { files: [jpegFile] } });
+            expect(jpegFile.type === 'image/jpeg').toBeTruthy();
+  
+
+            const form = screen.getByTestId("form-new-bill");
+            const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+            form.addEventListener("submit", handleSubmit);
+            fireEvent.submit(form);
+            expect(handleSubmit).toHaveBeenCalled()
+            expect(form).toBeTruthy();
+
+          })
+        })
+      })
     })
   })
 })
+
+
+
+
+
